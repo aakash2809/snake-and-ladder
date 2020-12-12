8f3ed442ad68f60da3c824ed5ed2c31c2684174c
aakash2809 <aakashrajak289@gmail.com>
2020-12-12 09:28:40 +0530

    Added: snake and ladder functionality with iteration

--- SnakeAndLadderGame.js
+++ SnakeAndLadderGame.js
@@ -1,40 +1,47 @@
 var playerPosition = 0;
 
-// ladder functionality
+// Ladder functionality
 ladder = (diceRollResult) => playerPosition + diceRollResult;
 
-// snake functionality
+// Snake functionality
 snake = (diceRollResult) => playerPosition - diceRollResult;
 
 /**
- *@description Here Snake and Ladder game is initiated for the single player
- */
+ *@description Here Snake and Ladder game for the sing player
+ *loop will itterate till 100 position
+ * */
+while (playerPosition <= 100) {
+    console.log("welcome to Snake and Ladder Game");
+    console.log("dice is rolling to get number....");
+    diceRollResult = (Math.floor(Math.random() * 10) % 6 + 1);
+    console.log("dice rolling result is- " + diceRollResult);
 
-console.log("welcome to Snake and Ladder Game");
-console.log("dice is rolling to get number....");
-diceRollResult = (Math.floor(Math.random() * 10) % 6 + 1);
-console.log("dice rolling result is- " + diceRollResult);
+    optionToPlay = (Math.floor(Math.random() * 10) % 3);
 
-optionToPlay = (Math.floor(Math.random() * 10) % 3);
-
-		// Snake and Ladder Evaluation
-		switch (optionToPlay) {
-		case 0:
-			currentPosition = playerPosition;
-			console.log("no play");
-			console.log("updated player positon- " + currentPosition);
-			break;
-		case 1:
-			currentPosition = ladder(diceRollResult);
-			console.log("found ladder");
-			console.log("updated player positon- " + currentPosition);
-			break;
-		case 2:
-			currentPosition = snake(diceRollResult);
-			console.log("found snake ");
-			console.log("updated player positon- " + currentPosition);
-			break;
-		default:
+    // Snake and Ladder Evaluation
+    switch (optionToPlay) {
+        case 0:
+           // currentPosition = playerPosition;
+            console.log("no play");
+            console.log("updated player positon- " + playerPosition);
+            break;
+        case 1:
+            playerPosition = ladder(diceRollResult);
+            console.log("found ladder");
+            console.log("updated player positon- " + playerPosition);
+            break;
+        case 2:
+            playerPosition = snake(diceRollResult);
+            console.log("found snake ");
+            console.log("updated player positon- " + playerPosition);
+            break;
+        default:
             console.log("Option did not match");
-		}
-	
+    }
+
+    /**
+    * Checking the player position whether it is less than 0 
+    * then do some action 
+    */
+    (playerPosition < 0) ? playerPosition = 0 : playerPosition;
+}
