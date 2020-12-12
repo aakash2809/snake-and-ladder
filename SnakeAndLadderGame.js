4ad8bc988ff4377000777835ebbbb979cae7eaeb
aakash2809 <aakashrajak289@gmail.com>
2020-12-12 07:47:06 +0530

    Added:  Roll dice to get number

--- SnakeAndLadderGame.js
+++ SnakeAndLadderGame.js
@@ -3,3 +3,6 @@
  */
 var playerPosition = 0;
 console.log("welcome to Snake and Ladder Game");
+console.log("dice is rolling to get number....");
+diceRollResult = (Math.floor(Math.random() * 10) % 6 + 1);
+console.log("dice rolling result is- " + diceRollResult);
