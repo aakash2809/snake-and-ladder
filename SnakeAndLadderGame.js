047c08fb23c4db478b4159acf71e24d636840a8c
aakash2809 <aakashrajak289@gmail.com>
2020-12-12 10:18:17 +0530

    Add : repeate moves till winning position reached and count number of rolls

--- SnakeAndLadderGame.js
+++ SnakeAndLadderGame.js
@@ -1,3 +1,7 @@
+const WIN_POSITION = 100;
+const START_POSITION = 0;
+
+var numberOfRolls = 0;
 var playerPosition = 0;
 var previousPosition = 0;
 
@@ -9,47 +13,47 @@ snake = (diceRollResult) => playerPosition - diceRollResult;
 
 /**
  *@description Here Snake and Ladder game for the single player
- *loop will itterate till 100 position
+ *loop will itterate till WIN_POSITION position
  * */
-while (playerPosition != 100) {
-    previousPosition = playerPosition;
-    console.log("welcome to Snake and Ladder Game");
-    console.log("dice is rolling to get number....");
-    diceRollResult = (Math.floor(Math.random() * 10) % 6 + 1);
-    console.log("dice rolling result is- " + diceRollResult);
+main = () => {
+    while (playerPosition != WIN_POSITION) {
+        previousPosition = playerPosition;
+        console.log("welcome to Snake and Ladder Game");
+        console.log("dice is rolling to get number....");
+        diceRollResult = (Math.floor(Math.random() * 10) % 6 + 1);
+        console.log("dice rolling result is- " + diceRollResult);
 
-    optionToPlay = (Math.floor(Math.random() * 10) % 3);
+        optionToPlay = (Math.floor(Math.random() * 10) % 3);
 
-    // Snake and Ladder Evaluation
-    switch (optionToPlay) {
-        case 0:
-            console.log("no play");
-            console.log("updated player positon- " + playerPosition);
-            break;
-        case 1:
-            playerPosition = ladder(diceRollResult);
-            console.log("found ladder");
-            console.log("updated player positon- " + playerPosition);
-            break;
-        case 2:
-            playerPosition = snake(diceRollResult);
-            console.log("found snake ");
-            console.log("updated player positon- " + playerPosition);
-            break;
-        default:
-            console.log("Option did not match");
+        // Snake and Ladder Evaluation
+        switch (optionToPlay) {
+            case 0:
+                console.log("no play");
+                console.log("updated player positon- " + playerPosition);
+                break;
+            case 1:
+                playerPosition = ladder(diceRollResult);
+                console.log("found ladder");
+                console.log("updated player positon- " + playerPosition);
+                break;
+            case 2:
+                playerPosition = snake(diceRollResult);
+                console.log("found snake ");
+                console.log("updated player positon- " + playerPosition);
+                break;
+            default:
+                console.log("Option did not match");
+        }
+
+        (playerPosition < START_POSITION) ? playerPosition = 0 : playerPosition;
+        (playerPosition > WIN_POSITION) ? playerPosition = previousPosition : playerPosition;
+
+        numberOfRolls++;
     }
-
-    /**
-    * Checking the player position whether it is less than 0 
-    * if yes then do some action 
-    */
-    (playerPosition < 0) ? playerPosition = 0 : playerPosition;
-
-    /**
-  * Checking the player position whether it is greater than 100 
-  * if yes then do some action 
-  */
-    (playerPosition > 100) ? playerPosition = previousPosition : playerPosition;
-
 }
+
+main();
+console.log("-----------------------------------------------------------");
+console.log();
+console.log("Player have won the game ");
+console.log("Number of dice rolls taken to win " + numberOfRolls);
