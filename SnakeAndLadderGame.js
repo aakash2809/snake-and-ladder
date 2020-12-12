810fe74e8240f0a5c0b3c17c0aa1dc0b3bbeee07
aakash2809 <aakashrajak289@gmail.com>
2020-12-12 07:36:43 +0530

    Game initiated with player position

--- SnakeAndLadderGame.js
+++ SnakeAndLadderGame.js
@@ -1,1 +1,5 @@
-console.log( "welcome to Snake and Ladder Game");
+/**
+ *@description Here Snake and Ladder game is initiated for the single player
+ */
+var playerPosition = 0;
+console.log("welcome to Snake and Ladder Game");
